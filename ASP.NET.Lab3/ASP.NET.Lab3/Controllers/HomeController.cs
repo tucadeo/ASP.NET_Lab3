af9f010fa6459823c1f41b720461a0c19548de6f
Krzysztof Talar <kt054489@student.ath.edu.pl>
2021-03-18 17:29:11 +0100

    Page created

---
+++ ASP.NET.Lab3/ASP.NET.Lab3/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ASP.NET.Lab3.Models;
+
+namespace ASP.NET.Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            const string url = "https://www.techradar.com/rss";
+
+            var xml = XElement.Load(url);
+
+            var items = xml.Descendants("item")
+                .Select(n => new RssItem
+                {
+                    Title = n.Element("title")?.Value,
+                    Description = n.Element("description")?.Value,
+                    Image = n.Descendants("image")
+                        .Select(x => x.Element("url")?.Value)
+                        .First(),
+                    Link = n.Element("link")?.Value,
+                })
+                .Take(4)
+                .AsQueryable();
+
+            return View(items);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET.Lab3/ASP.NET.Lab3/Models/RssItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NET.Lab3.Models
+{
+    public class RssItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image  { get; set; }
+        public string Link  { get; set; }
+    }
+}
